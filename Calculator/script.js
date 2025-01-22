e8083543f224d79810550f400d2c3c4b5c0e83cd
HAREESH VS <149133287+deesports@users.noreply.github.com>
2025-01-22 22:38:28 +0530

    Delete Calculator directory

--- Calculator/script.js
+++
@@ -1,9 +0,0 @@
-function clcikbtn(val){
-    document.getElementById("display").value += val;
-}
-function clearbtn(){
-    document.getElementById("display").value = "";
-}
-function equalbtn(){
-    document.getElementById("display").value = eval(document.getElementById("display").value); 
-}
